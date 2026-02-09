8e41e40d3226b4fdddb4d595c3cc4cce6f89516b
Southern Just <karuguezekiel@gmail.com>
2026-02-09 23:31:48 +0300

    Commit 001

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">MPESA sTATs!</h1>
-      <p className="mt-4 text-lg">hELLO wOrLd page.</p>
+      <h1 className="text-4xl font-bold">MPESA sTATs</h1>
+      <p className="mt-4 text-lg">hELLO wOrLd !</p>
     </main>
   );
 }
